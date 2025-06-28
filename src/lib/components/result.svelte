47eebba228e7e8d6115e9f6dc816fef5fb5ce9c2
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-28 02:12:32 -0300

    06-Getting the Result
    06-Getting the Result

--- src/lib/components/result.svelte
+++ src/lib/components/result.svelte
@@ -1,10 +1,38 @@
 <script>
+    import { onMount } from "svelte";
     let {store} = $props();
 
+    const getRandomName = () => {
+        return $store.names[Math.floor(Math.random() * $store.names.length)]
+    }
+
+    const generateResult = () => {
+        let rand = getRandomName();
+
+            /// CODE
+
+        $store.result = rand;
+    }
+
+    onMount(()=>{
+        generateResult()
+    })
 </script>
 
 <div class="container">
     <div class="result_container">
         <h1>The looser is:</h1>
+        <div class="result_value">
+            {$store.result}
+        </div>
+
+        <button class="action_button">
+            Start over
+        </button>
+        <br/>
+        <button class="action_button btn2">
+            Get a new name
+        </button>
+
     </div>
 </div>
