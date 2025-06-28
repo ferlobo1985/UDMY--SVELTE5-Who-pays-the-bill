31a3332631b24b9169faba7b340a0e8b9a3c321c
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-28 01:13:47 -0300

    02-Adding names to the list
    02-Adding names to the list

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,11 @@
     import store from './store/app.store'
     import Names from "./lib/components/names.svelte";
     import Result from "./lib/components/result.svelte";
+
+    store.subscribe((obj)=>{
+      console.log(obj)
+    })
+
 </script>
 
 {#if $store.state}
--- src/lib/components/names.svelte
+++ src/lib/components/names.svelte
@@ -1,10 +1,32 @@
 <script>
     let {store} = $props();
 
+    const addNamesToList = () => {
+        const userName = $store.inputName;
+
+        $store.names.push(userName)
+        $store.inputName = '';
+    }
+
 </script>
 
 <div class="container">
     <div class="logo">
         <h1>Who pays the bill</h1>
     </div>
+
+    <div class="input_container">
+        <input type="text" bind:value={$store.inputName}/>
+        <button onclick={addNamesToList}>Add</button>
+    </div>
+
+
+    <div class="list_of_names">
+        {#each $store.names as name, index(name) }
+            <button>
+                {name}
+            </button>
+        {/each}
+    </div>
+
 </div>
